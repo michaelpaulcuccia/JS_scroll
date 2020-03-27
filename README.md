f86caa670e7221a0ac13af7042e6d5f04aa20421
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-03-27 10:51:21 -0400

    setTimeout

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JS_scroll
+
+--https://jsonplaceholder.typicode.com/posts?_limit=3
+--https://jsonplaceholder.typicode.com/posts?_limit=3&_page=2
---
+++ main.js
@@ -0,0 +1,74 @@
+const postsContainer = document.getElementById('posts-container');
+const loading = document.querySelector('.loader');
+const filter = document.getElementById('filter');
+
+let limit = 5;
+let page = 1;
+
+//fetch posts from API
+
+async function getPosts() {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
+
+    const data = await res.json();
+
+    return data;
+}
+
+//show posts in DOM
+
+async function showPosts() {
+    const posts = await getPosts();
+
+    console.log(posts);
+
+    posts.forEach(post => {
+        const postEl = document.createElement('div');
+        postEl.classList.add('post');
+        postEl.innerHTML = 
+        `<div class='number'>${post.id}</div>
+        <div class='post-info'>
+            <h2 class='post-title'>${post.title}</h2>
+            <p class='post-body'>${post.body}</p>
+        </div>`;
+
+        postsContainer.appendChild(postEl);
+    });
+}
+
+showPosts();
+
+
+//Show Loader and Fetch More Posts
+
+function showLoading() {
+
+    //CSS - .loader.show {opacity: 1;}
+
+    loading.classList.add('show')
+    setTimeout( () => {
+        loading.classList.remove('show')
+
+            setTimeout( () => {
+                page++;
+                showPosts();
+            }, 300);
+
+    }, 1000);
+}
+
+
+window.addEventListener('scroll', () => {
+    //console.log(document.documentElement.scrollTop);
+    //console.log(document.documentElement.scrollHeight);
+    //console.log(document.documentElement.clientHeight);
+
+    const { scrollTop, scrollHeight, clientHeight} = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+        //console.log(123);
+
+        showLoading();
+
+    }
+})
